c7c9695761a1e7b1c0c87962d96f5f6b9b773412
Alex Petrov <alexp@coffeenco.de>
2014-10-22 11:07:49 +0200

    Bits and pieces for argv passing (possibly? )

--- cbits/wrapper.c
+++ cbits/wrapper.c
@@ -1,9 +1,19 @@
 #include "stdio.h"
-#include "HsFFI.h"
+/* #include "HsFFI.h" */
+#include "Rts.h"
 #include "C_stub.h"
 
 void
 module_register (void) {
-        hs_init ((void*)0, (void*)0);
-        initializeHaskell();
+        /* char *arr[1] = { "-N2" }; */
+        /* char **arr_cast = arr; */
+        /* int argc = 1; */
+
+        // hs_init (i, myArray);
+
+        /* printf("%s", "trying to turn on rts"); */
+
+        /* hs_init(&argc, &arr_cast); */
+        hs_init((void*)0, (void*)0);
+        /* initializeHaskell(); */
 }
