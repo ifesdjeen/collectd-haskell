f6d7bc85b517dd8cdf5a220f6d5d126725ee3419
Alex Petrov <oleksandr.petrov@gmail.com>
2014-12-15 20:59:31 +0100

    Update the way values are sent to the continuum server

--- src/Collectd/ContinuumTest.hs
+++ src/Collectd/ContinuumTest.hs
@@ -14,7 +14,9 @@ import           Control.Concurrent.STM
 --import qualified Data.ByteString       as B
 
 import           Collectd.Types
-import           Control.Monad         ( when )
+import           Control.Monad          ( when, forM_ )
+import           Data.Maybe             ( fromMaybe )
+
 import qualified Collectd.C            as C
 import qualified Data.ByteString.Char8 as B
 import qualified Data.Map              as Map
@@ -27,53 +29,58 @@ toDbValue (AbsoluteT  v) = Continuum.DbLong (fromIntegral v)
 toDbValue _ = undefined
 
 write :: Continuum.ContinuumClient
-         -> TVar [(DbValue,FieldName,DbValue)]
-         -> TVar Integer
+         -> TVar (Map.Map (DbValue,B.ByteString,Integer) [(FieldName, DbValue)])
+         -> TVar (Maybe Integer)
          -> C.WriteCallbackFn
 
 write client state flushCounter dataSet valueList userData = do
   values    <- C.unpackValueList dataSet valueList
 
-  -- print $ values
+  let timestamp  = fromIntegral $ vTime values
 
-  let sendData ValueList{..} =
-        mapM (\(_, v) ->
-               (swap state
-                (\oldSt ->
-                  (DbString (B.pack vHost),
-                   B.pack vTypeInstance,
-                   toDbValue v) : oldSt)))
-        (Map.toList vValues)
-  _         <- sendData values
+  valuesToFlush <- atomically $ do
+    maybeOldCounter <- readTVar flushCounter
+    oldValues       <- readTVar state
+    _               <- modifyTVar flushCounter (swapCounter timestamp)
 
+    case maybeOldCounter of
+      (Just oldTimestamp) | timestamp > oldTimestamp -> do
+        _ <- writeTVar state (Map.empty)
+        _ <- enqueue state values
+        return (Just oldValues)
+      _                                              -> do
+        _ <- enqueue state values
+        return Nothing
 
-  -- let sendData ValueList{..} =
-  --       mapM (\(_, v) ->
-  --              (swap state
-  --               (\oldSt ->
-  --                 (Continuum.Insert
-  --                  (B.pack vPluginInstance)
-  --                  (Continuum.makeRecord
-  --                   (fromIntegral vTime)
-  --                   [("host",    DbString (B.pack vHost))
-  --                   ,("subtype", DbString (B.pack vTypeInstance))
-  --                   ,("value",   toDbValue v)]))
-  --                 : oldSt
-  --               )))  (Map.toList vValues)
-  -- _         <- sendData values
-
-  -- _ <- swap flushCounter (+ 1)
-  -- currentCounter <- atomRead flushCounter
-
-  -- when (currentCounter > 50) (flush client state flushCounter)
-
-
+  print $ valuesToFlush
+  -- _ <- flush client valuesToFlush
 
   return 0
 
+  where enqueue state ValueList{..}    =
+          modifyTVar state (Map.alter
+                            (append $ map (\x -> (B.pack vTypeInstance, x)) $ map toDbValue (Map.elems vValues))
+                            (DbString $ B.pack vHost,
+                             B.pack vPluginInstance,
+                             fromIntegral vTime))
+        append x Nothing          = Just x
+        append x (Just old)       = Just (x ++ old)
 
--- flush :: Continuum.ContinuumClient -> TVar [Continuum.Request] -> TVar Integer -> IO ()
--- flush client state flushCounter = do
+        swapCounter newv Nothing  = Just newv
+        swapCounter newv (Just _) = Just newv
+
+flush :: Continuum.ContinuumClient
+         -> Maybe (Map.Map (DbValue,B.ByteString,Integer) [(FieldName, DbValue)])
+         -> IO ()
+flush client (Just values) = do
+  _ <- forM_ (Map.toList values) $ \((host, collection, timestamp), values) ->
+    Continuum.sendRequest client $
+    Continuum.Insert collection $
+    Continuum.makeRecord timestamp (("host", host) : values)
+
+  return ()
+flush _ _ = return ()
+
 --   requests  <- atomRead state
 --   _         <- atomReset state []
 --   _         <- atomReset flushCounter 0
@@ -99,28 +106,49 @@ configCallback config = do
 
 memorySchema =
   Continuum.makeSchema [ ("host",    Continuum.DbtString)
-                       , ("value",   Continuum.DbtLong)
-                       , ("subtype", Continuum.DbtString) ]
+                       , ("actual_used",   Continuum.DbtLong)
+                       , ("actual_free",   Continuum.DbtLong)
+                       , ("used",   Continuum.DbtLong)
+                       , ("free",   Continuum.DbtLong)
+                       , ("total",   Continuum.DbtLong)
+                       , ("ram",   Continuum.DbtLong) ]
 
 memoryPercentSchema =
   Continuum.makeSchema [ ("host",    Continuum.DbtString)
-                       , ("value",   Continuum.DbtDouble)
-                       , ("subtype", Continuum.DbtString) ]
+                       , ("used_percent",   Continuum.DbtDouble)
+                       , ("free_percent",   Continuum.DbtDouble)]
 
 cpuSchema =
   Continuum.makeSchema [ ("host",    Continuum.DbtString)
-                       , ("value",   Continuum.DbtDouble)
-                       , ("subtype", Continuum.DbtString) ]
+                       , ("combined",   Continuum.DbtDouble)
+                       , ("stolen",   Continuum.DbtDouble)
+                       , ("soft_irq",   Continuum.DbtDouble)
+                       , ("irq",   Continuum.DbtDouble)
+                       , ("idle",   Continuum.DbtDouble)
+                       , ("wait",   Continuum.DbtDouble)
+                       , ("user",   Continuum.DbtDouble)
+                       , ("sys",   Continuum.DbtDouble)
+                       , ("nice",   Continuum.DbtDouble)]
 
 networkSchema =
   Continuum.makeSchema [ ("host",    Continuum.DbtString)
-                       , ("value",   Continuum.DbtLong)
-                       , ("subtype", Continuum.DbtString) ]
+                       , ("all_outbound_total",   Continuum.DbtLong)
+                       , ("all_inbound_total",   Continuum.DbtLong)
+                       , ("tcp_outbound_total",   Continuum.DbtLong)
+                       , ("tcp_inbound_total",   Continuum.DbtLong) ]
 
 tcpSchema =
   Continuum.makeSchema [ ("host",    Continuum.DbtString)
-                       , ("value",   Continuum.DbtLong)
-                       , ("subtype", Continuum.DbtString) ]
+                       , ("out_rsts",   Continuum.DbtLong)
+                       , ("in_errs",   Continuum.DbtLong)
+                       , ("retrans_segs",   Continuum.DbtLong)
+                       , ("out_segs",   Continuum.DbtLong)
+                       , ("in_segs",   Continuum.DbtLong)
+                       , ("curr_estab",   Continuum.DbtLong)
+                       , ("estab_resets",   Continuum.DbtLong)
+                       , ("attempt_fails",   Continuum.DbtLong)
+                       , ("passive_opens",   Continuum.DbtLong)
+                       , ("active_opens",   Continuum.DbtLong) ]
 
 foreign export ccall module_register :: IO ()
 
@@ -135,8 +163,8 @@ module_register = do
 
   userData <- makeUserData (C.Custom "custom name" 100)
 
-  flushState   <- atomically $ newTVar []
-  flushCounter <- atomically $ newTVar 0
+  flushState   <- atomically $ newTVar Map.empty
+  flushCounter <- atomically $ newTVar Nothing
 
   writeFn  <- C.makeWriteCallbackFn (write client flushState flushCounter)
   configFn <- C.makeConfigCallbackFn configCallback
